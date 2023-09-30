dd969a99f1bb38aad74e28ac40b691923685f766
Atul Patil <145986637+AtulPatil05@users.noreply.github.com>
2023-09-30 09:35:34 +0530

    Add files via upload

---
+++ Display100Numbers.java
@@ -0,0 +1,17 @@
+package com.prowings.loops;
+
+class Display100Numbers
+{
+	public static void displayNumbers()
+	{
+		int i;
+		for(i=1;i<=100;i++)
+		{
+			System.out.println(i);
+		}
+	}
+	public static void main(String[] args)
+	{
+		displayNumbers();
+	}
+}
