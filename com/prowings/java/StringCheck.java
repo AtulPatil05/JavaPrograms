4732718959726a00baa6dba54a4196a6ed9a437d
Atul-Patil <atulpatil051099@gmail.com>
2023-10-09 23:10:20 +0530

    String Check

--- com/prowings/java/StringCheck.java
+++ com/prowings/java/StringCheck.java
@@ -10,13 +10,11 @@ public class StringCheck {
 		String s = sc.nextLine();
 		System.out.println("Enter Boolean Value");
 		boolean b = sc.nextBoolean();
-		
 		System.out.println(stringChecker(s, b));
 	}
 
 	private static char stringChecker(String s, boolean b) {
-		char[] chArry = s.toCharArray();
-		return (b == true) ? chArry[0] : chArry[chArry.length - 1];
+		return (b == true) ? s.charAt(0) : s.charAt(s.length() - 1);
 	}
 
 }
