17b909a15b3ebe35d06b1e1b1d02e91a85770804
Atul-Patil <atulpatil051099@gmail.com>
2023-12-14 19:37:16 +0530

    Find num Is Even or Odd

--- com/prowings/java/EvenOrOdd.java
+++ com/prowings/java/EvenOrOdd.java
@@ -1,26 +1,24 @@
 package com.prowings.java;
+
 import java.util.*;
-class EvenOrOdd
-{
-	public static String isEvenOrNot(int number)
-	{
-		if(number%2==0)
-		{
-			if (number!=0)
-			  return "Number is Even";
-		    else
-			  return "Number is Neutral";
-		}
-		else
-		{
-		  return "Number is Odd";
+
+class EvenOrOdd {
+	static Scanner sc = new Scanner(System.in);
+
+	public static String isEvenOrNot(int number) {
+		if (number % 2 == 0) {
+			if (number != 0)
+				return "Number is Even";
+			else
+				return "Number is Neutral";
+		} else {
+			return "Number is Odd";
 		}
 	}
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
 		System.out.println("Enter Number ");
 		int number = sc.nextInt();
-		System.out.println(number+ " " +isEvenOrNot(number));
+		System.out.println(number + " " + isEvenOrNot(number));
 	}
 }
