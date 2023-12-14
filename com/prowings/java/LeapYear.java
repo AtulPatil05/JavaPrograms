5d037eb9b363aab1a90d8b3d172896443e4e703f
Atul-Patil <atulpatil051099@gmail.com>
2023-12-14 19:41:04 +0530

    Leap Year

--- com/prowings/java/LeapYear.java
+++ com/prowings/java/LeapYear.java
@@ -1,18 +1,19 @@
 package com.prowings.java;
+
 import java.util.Scanner;
-class LeapYear
-{
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		int year = sc.nextInt(); 
-		if(leapYear(year))
+
+class LeapYear {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int year = sc.nextInt();
+		if (leapYear(year))
 			System.out.println("Year is Leap");
 		else
 			System.out.println("Year is Not Leap");
 	}
-	public static boolean leapYear(int year)
-	{
-		return (year % 4 == 0)? ((year % 100 == 0) && (year % 400 == 0)) ? true : false: false;
+
+	public static boolean leapYear(int year) {
+		return (year % 4 == 0) ? ((year % 100 == 0) && (year % 400 == 0)) ? true : false : false;
 	}
 }
