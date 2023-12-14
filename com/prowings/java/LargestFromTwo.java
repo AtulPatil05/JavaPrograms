4713b0670bd998e36024e88c5ac6cdcf3f4e768c
Atul-Patil <atulpatil051099@gmail.com>
2023-12-14 19:40:22 +0530

    Largest From Two

--- com/prowings/java/LargestFromTwo.java
+++ com/prowings/java/LargestFromTwo.java
@@ -1,28 +1,26 @@
 package com.prowings.java;
+
 import java.util.*;
-class LargestFromTwo
-{
-	public static String largestNumber(int num1,int num2)
-	{
-		if(num1>=num2)
-		{
-			if(num1>num2)
+
+class LargestFromTwo {
+	public static String largestNumber(int num1, int num2) {
+		if (num1 >= num2) {
+			if (num1 > num2)
 				return "Num1 is Largest";
 			else
 				return "num1 and num2 Both are Same";
-		}
-		else
-		{
-		  return "Num2 is Largest";
+		} else {
+			return "Num2 is Largest";
 		}
 	}
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
 		int num1 = sc.nextInt();
-		System.out.println("Enter num1 is :- "+num1);
+		System.out.println("Enter num1 is :- " + num1);
 		int num2 = sc.nextInt();
-		System.out.println("Enter num2 is :- "+num2);
-		System.out.println(largestNumber(num1,num2));
+		System.out.println("Enter num2 is :- " + num2);
+		System.out.println(largestNumber(num1, num2));
 	}
 }
