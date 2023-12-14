6ff187ec52373dcf650157a0e0002dd7cd689790
Atul-Patil <atulpatil051099@gmail.com>
2023-12-14 19:46:08 +0530

    Swap Two Nums

--- com/prowings/java/SwappingNumbers.java
+++ com/prowings/java/SwappingNumbers.java
@@ -2,22 +2,22 @@
 package com.prowings.java;
 
 import java.util.Scanner;
-class SwappingNumbers
-{
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
+
+class SwappingNumbers {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
 		System.out.println("Before Swapping Numbers");
-		System.out.println("Number A  :- "+num1);
-		System.out.println("Number B  :- "+num2);
-		
-		num1 = num1+num2;
-		num2 = num1-num2;
-		num1 = num1-num2;
+		System.out.println("Number A  :- " + num1);
+		System.out.println("Number B  :- " + num2);
+
+		num1 = num1 + num2;
+		num2 = num1 - num2;
+		num1 = num1 - num2;
 		System.out.println("After Swapping Numbers");
-		System.out.println("Number A  :- "+num1);
-		System.out.println("Number B  :- "+num2);
+		System.out.println("Number A  :- " + num1);
+		System.out.println("Number B  :- " + num2);
 	}
 }
