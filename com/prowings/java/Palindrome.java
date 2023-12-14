1454b4d1c6aa6f0d7e9355a4068505bd8eca9f58
Atul-Patil <atulpatil051099@gmail.com>
2023-12-14 19:43:23 +0530

    Palindrome

--- com/prowings/java/Palindrome.java
+++ com/prowings/java/Palindrome.java
@@ -1,27 +1,31 @@
 package com.prowings.java;
+
 import java.util.Scanner;
-class Palindrome{
-	public static void palindrome(int i){
+
+class Palindrome {
+	public static void palindrome(int i) {
 		int j = i;
 		int res = 0;
 		int rem = 0;
-		 while (i != 0) {
-      rem = i % 10;
-      res = res * 10 + rem;
-      i = i /  10;
-	  System.out.println(res);
-    }
-	if (res == j)
-		 System.out.println("Number is Palindrome");
-    else
-		 System.out.println("Number is Not-Palindrome");
+		while (i != 0) {
+			rem = i % 10;
+			res = res * 10 + rem;
+			i = i / 10;
+			System.out.println(res);
+		}
+		if (res == j)
+			System.out.println("Number is Palindrome");
+		else
+			System.out.println("Number is Not-Palindrome");
 	}
-public static void main(String[] args){
-	Scanner sc = new Scanner(System.in);
-	int j = sc.nextInt();
-palindrome(j);
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int j = sc.nextInt();
+		palindrome(j);
+	}
 }
-}	
  /*D:\ME>java Palindrome
 123321
 1
