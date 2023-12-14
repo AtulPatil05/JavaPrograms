dda8b09201592234fdb9fb527599a9b7f3696291
Atul-Patil <atulpatil051099@gmail.com>
2023-12-14 19:46:43 +0530

    Calculator using Switch

--- com/prowings/java/SwitchCaseCalculator.java
+++ com/prowings/java/SwitchCaseCalculator.java
@@ -1,41 +1,41 @@
 package com.prowings.java;
+
 import java.util.*;
-class SwitchCaseCalculator
-{
-	public static float calculator(float num1, float num2, String operator)
-	{
+
+class SwitchCaseCalculator {
+	public static float calculator(float num1, float num2, String operator) {
 		float f = 1.0f;
 		String s = operator.toLowerCase();
-		switch(s)
-		{
-			case "+":
-			return num1+num2;
-			
-			case "-":
-			return num1-num2;
-			
-			case "*":
-			return num1*num2;
-			
-			case "/":
-			return num1/num2;
-			
-			case "%":
-			return num1%num2;
-			
-			default:
+		switch (s) {
+		case "+":
+			return num1 + num2;
+
+		case "-":
+			return num1 - num2;
+
+		case "*":
+			return num1 * num2;
+
+		case "/":
+			return num1 / num2;
+
+		case "%":
+			return num1 % num2;
+
+		default:
 			return f;
 		}
 	}
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
 		System.out.println("Enter num1");
 		float num1 = sc.nextFloat();
 		System.out.println("Enter num2");
 		float num2 = sc.nextFloat();
 		System.out.println("Enter Operator to perform");
 		String operator = sc.next();
-		System.out.println("Result is :- "+calculator(num1,num2,operator));
+		System.out.println("Result is :- " + calculator(num1, num2, operator));
 	}
 }
