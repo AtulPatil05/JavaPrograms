488c70c3d4de61f7320441db53ac6732786ea9cd
Atul-Patil <atulpatil051099@gmail.com>
2023-12-14 19:43:56 +0530

    Print Pass or Fail

--- com/prowings/java/PassFail.java
+++ com/prowings/java/PassFail.java
@@ -1,23 +1,21 @@
 package com.prowings.java;
+
 import java.util.*;
-class PassFail
-{
-	public static String marks(int marks)
-	{
-		if(marks>=35)
-		{
-				return "Pass";
-		}
-		else
-		{
-		  return "Fail";
+
+class PassFail {
+	public static String marks(int marks) {
+		if (marks >= 35) {
+			return "Pass";
+		} else {
+			return "Fail";
 		}
 	}
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
 		System.out.println("Enter Marks :- ");
 		int marks = sc.nextInt();
-		System.out.println("Remark :- "+marks(marks));
+		System.out.println("Remark :- " + marks(marks));
 	}
 }
