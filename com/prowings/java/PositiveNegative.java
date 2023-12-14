e0be168cee7cdc43a1e0c1f6777df7eb4d0b575f
Atul-Patil <atulpatil051099@gmail.com>
2023-12-14 19:44:56 +0530

    Find Particulre num is +tive or -tive

--- com/prowings/java/PositiveNegative.java
+++ com/prowings/java/PositiveNegative.java
@@ -1,31 +1,26 @@
 package com.prowings.java;
+
 import java.util.*;
-class PositiveNegative
-{
-	public static String isPositiveOrNot(int num)
-	{
-		if(num>=0)
-		{
-			if(num==0)
-			{
+
+class PositiveNegative {
+	public static String isPositiveOrNot(int num) {
+		if (num >= 0) {
+			if (num == 0) {
 				return "Number is Zero";
-			}
-			else
-			{
+			} else {
 				return "Number is Positive";
 			}
+		} else {
+			return "Number is Negative";
 		}
-		else
-		{
-				return "Number is Negative";
-		}
-		
+
 	}
-	public static void main(String[] args)
-	{
-	Scanner sc =  new Scanner(System.in);
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
 		int num = sc.nextInt();
-		System.out.println(num+":- "+isPositiveOrNot(num));
+		System.out.println(num + ":- " + isPositiveOrNot(num));
 	}
-	
+
 }
