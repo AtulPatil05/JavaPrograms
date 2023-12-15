a1231d408a9da834328d6f4a0a5a74f7cb090474
Atul-Patil <atulpatil051099@gmail.com>
2023-12-15 22:10:13 +0530

    Find Duplicates

--- com/prowings/array/FindDuplicates.java
+++ com/prowings/array/FindDuplicates.java
@@ -28,6 +28,7 @@ public class FindDuplicates {
 		}
 		int count = 0;
 		for (int i = 0; i < arr.length; i++) {
+
 			for (int j = i + 1; j < arr.length; j++) {
 				if (arr[i] == arr2[j]) {
 					count++;
